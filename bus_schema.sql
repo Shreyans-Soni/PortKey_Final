40ea0ac206035d37d9b2fd312ad252d4a096ede3
Rohit Kumar <34412543+rohit-malik@users.noreply.github.com>
2018-05-01 01:16:14 +0530

    adding files for Bus portal and database

--- bus_schema.sql
+++ bus_schema.sql
@@ -1,3 +1,6 @@
+create database portkey_bus;
+use portkey_bus;
+
 create table location (
 	location_id integer unsigned auto_increment not null unique,
 	longitude varchar(40),
@@ -13,33 +16,27 @@ create table authentication (
 	PRIMARY KEY(user_id)
 );
 
-create table session_info (
-	session_id integer unsigned not null unique,
-	location_id integer unsigned,
-	time varchar(40),
-	PRIMARY KEY (session_id),
-	FOREIGN KEY (location_id) REFERENCES location (location_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-create table bus (
-	bus_id integer unsigned not null unique auto_increment,
-	session_id integer unsigned,
-	bus_number integer unsigned not null,
-	colour varchar(40),
-	PRIMARY KEY(bus_id),
-	authenticated integer unsigned,
-	FOREIGN KEY (session_id) REFERENCES session_info (session_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
 
 create table user_info (
 	user_id integer unsigned not null unique,
-	bus_id integer unsigned,
-	location_id integer unsigned,
 	first_name varchar(40),
 	last_name varchar(40),
-	user_role integer unsigned not null,
+	user_role varchar(40) not null,
 	PRIMARY KEY (user_id),
-	FOREIGN KEY (user_id) REFERENCES authentication (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (bus_id) REFERENCES bus (bus_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (location_id) REFERENCES location (location_id) ON DELETE RESTRICT ON UPDATE CASCADE
+	FOREIGN KEY (user_id) REFERENCES authentication (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+
+create table bus (
+	bus_id integer unsigned not null unique auto_increment,
+	bus_number integer unsigned not null,
+	colour varchar(40),
+	Destination varchar(100),
+	PRIMARY KEY(bus_id),
+	location_id integer unsigned,
+	user_id integer unsigned,
+	FOREIGN KEY(location_id) REFERENCES location(location_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	FOREIGN KEY(user_id) REFERENCES user_info(user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
